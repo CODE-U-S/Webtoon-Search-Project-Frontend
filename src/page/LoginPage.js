bbc9341becfee75e39ea2224b62c4a40e9d76cca
MsEmily1020 <peace10200khs@gmail.com>
2023-12-29 13:18:10 +0900

    :lipstick: navbar 로고 & 웹옵션 & 검색창 생성

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Login from "../components/login/Login";
+import Navbar from "../components/navbar/Navbar";
 
 function LoginPage() {
   return (
     <div>
+      <Navbar />
       <Login />
     </div>
   );
