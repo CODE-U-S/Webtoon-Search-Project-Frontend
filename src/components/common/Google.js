7a87a8d849ecc399e3110cdcd17c9eebb5d24aed
MsEmily1020 <peace10200khs@gmail.com>
2023-12-30 20:10:48 +0900

    ✨ google sns 로그인 구현

---
+++ src/components/common/Google.js
@@ -0,0 +1,31 @@
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import styles from "../../css/common/Footer.module.css";
+import { jwtDecode } from "jwt-decode";
+
+function GoogleOAuth() {
+  return (
+    <div className={styles["sns-google"]}>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+        <GoogleLogin
+          onSuccess={(res) => {
+            handleCredentialResponse(res);
+          }}
+          onFailure={(res) => {
+            handleCredentialResponse(res);
+          }}
+          type="icon"
+          shape="circle"
+          useOneTap
+        />
+      </GoogleOAuthProvider>
+    </div>
+  );
+}
+
+function handleCredentialResponse(response) {
+  const { name, email } = jwtDecode(response.credential);
+  console.log(name);
+  console.log(email);
+}
+
+export default GoogleOAuth;
