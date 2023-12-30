f32df9f8a675a6fdedc70f0641df03f26acb645c
MsEmily1020 <peace10200khs@gmail.com>
2023-12-30 22:37:50 +0900

    💄 비밀번호 찾기 UI 완성

---
+++ src/page/FindPwPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import FindPw from "../components/auth/FindPw";
+import Footer from "../components/common/Footer";
+import Navbar from "../components/common/Navbar";
+
+function FindPwPage() {
+  return (
+    <div>
+      <Navbar />
+      <FindPw />
+      <Footer />
+    </div>
+  );
+}
+
+export default FindPwPage;
