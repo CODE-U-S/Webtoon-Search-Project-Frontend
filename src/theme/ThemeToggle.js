c0888c35d94eb5f97fda06fe5faff42eefb74e56
iris-starry <iris_starry_@naver.com>
2024-03-11 23:58:09 +0900

    style: 테마토클 스타일 변경

--- src/theme/ThemeToggle.js
+++ src/theme/ThemeToggle.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ToggleTab = styled.button`
+  width: 100px;
+  height: 35px;
+  border-radius: 15px;
+  background-color: ${({ mode }) => (mode === 'dark' ? '#333' : '#eee')};
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  color: ${({ mode }) => (mode === 'dark' ? '#eee' : '#333')};
+`;
 
 function ThemeToggle({ toggle, mode }) {
   return (
-    <button onClick={toggle}>
-      {mode === 'dark' ? '🌚' : '🌝'}
-    </button>
+    <ToggleTab onClick={toggle} mode={mode}>
+      {mode === 'dark' ? '라이트' : '다크'}
+    </ToggleTab>
   );
 }
 
