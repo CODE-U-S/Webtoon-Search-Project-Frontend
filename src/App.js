911e2da806f8c0fb87ca033db70b22f3281aa05b
iris-starry <iris_starry_@naver.com>
2024-04-04 23:30:07 +0900

    feat: DetailPage 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from './context/themeProvider';
+import { GlobalStyle } from './theme/GlobalStyles';
 import Home from './components/Home';
 import WebToon from './components/WebToon';
 import Favorite from './components/Favorite';
 import Signup from './pages/auth/Signup';
+import DetailPage from './pages/DetailPage';
 
-import { ThemeProvider } from './context/themeProvider';
-import { GlobalStyle } from './theme/GlobalStyles';
 
 // WebToon
 import MrBlue from './pages/webtoon/MrBlue';
@@ -28,6 +29,7 @@ function App() {
             <Route exact path="/WebToon" component={WebToon}/>
             <Route exact path="/Favorite" component={Favorite}/>
             <Route exact path="/Signup" component={Signup}/>
+            <Route exact path="/content" component={DetailPage}/>
 
             <Route exact path="/webToon/mrblue" component={MrBlue}/>
             <Route exact path="/webToon/anytoon" component={AnyToon}/>
